1a80559323aceb5c5be1088d786028b83f023ec0
Aly Soliman <alysoliman05@gmail.com>
2023-11-22 00:59:18 +0300

    tweak: small edits in `Author` and `Paper`

--- models/author.py
+++ models/author.py
@@ -1,31 +1,56 @@
 from models.user import User
 from models.paper import Paper
+from models.confernceAuthorRegisteration import ConferenceAuthorRegisteration
 
 import pickle
 
 class Author(User):
-       def __init__(self, userId, userName, password, name, dob, nationality):
+       def __init__(self, userId: int, userName: str, password: str, name: str, dob: str, nationality: str):
               super().__init__(userId, userName, password, name, dob, nationality)
        
-       def submitPaper(self, title: str, abstract: str, keywords: list, coAuthorsNames: list, confCode: int, document: str):
+       def submitPaper(self, title: str,paperNo: int, abstract: str, keywords: list, coAuthorsNames: list, confCode: int, document: str):
               try:
                      with open("data/papers.csv", "rb") as file:
                                papers = pickle.load(file)
-                     papers.append(Paper(self.userId, title, abstract, keywords, coAuthorsNames, confCode, document))
+                     papers.append(Paper(None, self.userId, paperNo, title, abstract, keywords, coAuthorsNames, False, None, document ))
                      with open("data/papers.csv", "wb") as file:
                             pickle.dump(papers, file)
                      print("Paper submitted successfully")
               
               except:
                      
-                     papers = [Paper(self.userId, title, abstract, keywords, coAuthorsNames, confCode, document)]
+                     papers = [Paper(None, self.userId, paperNo, title, abstract, keywords, coAuthorsNames, False, None, document )]
                      with open("data/papers.csv", "wb") as file:
                             pickle.dump(papers, file)
                      print("Paper submitted successfully")
        
-       def registerInConference(self, confCode: int, revisedPaperDocument: str, paperNo: int, creditCardDetails: str, mealChoice: str):
-              #todo 
-              return
+       def registerInConference(self, confName: str, paperTitle: str, creditCardDetails: str, mealChoice: str):
+              
+              papers = self.getAuthorPapers()
+    
+              for paper in papers:
+                     if paper.title == paperTitle:
+                            selectedPaper = paper
+                            paper.confName = confName
+                            paper.reviewed = True
+                            paper.systemDecision = 'Reviewed'
+                            break
+              
+              with open('data/papers.csv', 'wb') as f:
+                     pickle.dump(papers, f)
+              
+              
+              try:
+                     with open('data/registeredConferences.csv', 'rb') as f:
+                            conferences = pickle.load(f)
+                     conferences.append(ConferenceAuthorRegisteration(confName, self.userId,selectedPaper.paperNo, mealChoice, creditCardDetails, 'paid: 50$'))
+              except FileNotFoundError:
+                     conferences = []
+                     conferences.append(ConferenceAuthorRegisteration(confName, self.userId,selectedPaper.paperNo, mealChoice, creditCardDetails, 'paid: 50$'))
+              
+              with open('data/registeredConferences.csv', 'wb') as f:
+                     pickle.dump(conferences, f)
+                     
               
               
        
@@ -40,6 +65,19 @@ class Author(User):
                      return authorPapers
               except:
                      return []
+
+       def getRegisteredConferences(self):
+              try:
+                     with open('data/registeredConferences.csv', 'rb') as f:
+                            conferences = pickle.load(f)
+                     registeredConferences = []
+                     for conference in conferences:
+                            if conference.authorId == self.userId:
+                                   registeredConferences.append(conference)
+                     return registeredConferences
+              except FileNotFoundError:
+                     return []
+       
        
        
        
--- models/paper.py
+++ models/paper.py
@@ -1,14 +1,17 @@
 
 
 class Paper:
-       def __init__(self, authorId, title, abstract, keywords, coAuthorsNames, confCode, document):
+       def __init__(self,confName: str, authorId: int,paperNo: int, title: str, abstract: str, keywords, coAuthorsNames,reviewed: bool, systemDecision: str, document):
+              self.confName = confName
               self.authorId = authorId
+              self.paperNo = paperNo
               self.title = title
               self.abstract = abstract
               self.keywords = keywords
               self.coAuthorsNames = coAuthorsNames
-              self.confCode = confCode
+              self.reviewed = reviewed
+              self.systemDecision = systemDecision
               self.document = document
+              
        
-       def __str__ (self):
-              return f"{self.authorId} {self.title} {self.abstract} {self.keywords} {self.coAuthorsNames} {self.confCode} {self.document}"
+       
