e21fa5b2ed493e99d3dd15decd990e1272b17665
Aly Soliman <alysoliman05@gmail.com>
2023-11-22 01:00:50 +0300

    test: seed mock data

--- data/seed.py
+++ data/seed.py
@@ -1,11 +1,26 @@
 from models.conference import Conference
+from models.paper import Paper
+from models.author import Author
+from models.confernceAuthorRegisteration import ConferenceAuthorRegisteration
 import pickle
-conferences = [
-       Conference("ICML",['15/11/2023','20/11/2023'], '10/11/2023', 123),
-       Conference("ICLR",['15/11/2023','20/11/2023'], '10/11/2023', 124),
-       Conference("ICCV",['15/11/2023','20/11/2023'], '10/11/2023', 125),
+
+
+authors = [
+       
+       Author(1, 'alysoliman', 'pass123', 'Aly Soliman', '1/1/2000', 'Egyptian'),
+       Author(2, 'mohamed', 'pass123', 'Mohamed', '1/1/2000', 'Egyptian'),
+       Author(3, 'ahmed', 'pass123', 'Ahmed', '1/1/2000', 'Egyptian'),     
 ]
 
+with open('data/authors.dat', 'wb') as f:
+       pickle.dump(authors, f)
+
+
+conferences = [
+       Conference('ICSE', ['11/11/2023', '20/11/2023'], '06/11/2023', 1),
+       Conference('IEEE', ['11/11/2023', '20/11/2023'], '06/11/2023', 2),
+       Conference('ACM', ['11/11/2023', '20/11/2023'], '06/11/2023', 3),
+]
 
 with open('data/conferences.csv', 'wb') as f:
        pickle.dump(conferences, f)
