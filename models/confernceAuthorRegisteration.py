a3099bebefae420952785fac6df0b61281c46a9b
Aly Soliman <alysoliman05@gmail.com>
2023-11-22 00:58:41 +0300

    feat: create `ConferenceAuthorRegisteration` model

---
+++ models/confernceAuthorRegisteration.py
@@ -0,0 +1,11 @@
+
+
+class ConferenceAuthorRegisteration:
+    def __init__(self,confName, authorId,  acceptedPaperNo, creditCardDetails, mealChoice, reciept):
+        self.confName = confName
+        self.authorId = authorId
+        self.acceptedPaperNo = acceptedPaperNo
+        self.creditCardDetails = creditCardDetails
+        self.mealChoice = mealChoice
+        self.reciept = reciept
+    
