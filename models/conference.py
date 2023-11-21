3c4dd2fb35f8b9a299fe05862fd8bf27534ce536
Aly Soliman <alysoliman05@gmail.com>
2023-11-21 08:42:07 +0300

    feat: `Conference` class

---
+++ models/conference.py
@@ -0,0 +1,10 @@
+
+
+class Conference:
+       def __init__ (self, confName, dates, submissionDeadline, confCode):
+              self.confName = confName
+              self.dates = dates
+              self.submissionDeadline = submissionDeadline
+              self.confCode = confCode
+       
+              
